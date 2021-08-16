c757ca029a4a8b6bda9d292d165072f4e8bb67cc
sandeepan-sahoo <53312092+sandeepan-sahoo@users.noreply.github.com>
2021-08-16 18:00:03 +0530

    Add files via upload

---
+++ Assignment 1 Output.py
@@ -0,0 +1,19 @@
+Enter a natural number: 8
+The sum of first n natural numbers is 36
+enter the number of odd numbers whose sum you want to calculate: 3
+The sum of first n odd numbers is 9
+Enter the initial value of AP: 4
+Entere the term number of AP: 5
+The sum of 5 terms of AP is 35.0
+Enter the initial value of GP: 5
+Entere the term number of GP: 3
+The sum of 3 terms of GP is 8.75
+Enter the initial value of HP: 3
+Entere the term number of HP: 2
+The sum of 2 terms of HP is 4.2
+Enter the number whose factorial you wish to find: 4
+factorial of 4 is 24
+Enter small x: 0.08
+0.07991469396917268
+Enter small x: 0.9
+2.4596030065756698
